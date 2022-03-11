7b1ab2a1052e376bb2022053b1b98c08f8c88438
Carlos <carvagia92@gmail.com>
2022-03-11 16:06:26 +0100

    user done

--- appvr/classroomvr/admin.py
+++ appvr/classroomvr/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import *
 
 # Register your models here.
-class SchoolAdmin(admin.ModelAdmin):
+class SchoolInline(admin.StackedInline):
+    model = School
+    extra = 3
+
+class CourseAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['name']}),
-        (None, {'fields': ['location']}),
     ]
 
-admin.site.register(School, SchoolAdmin)
+UserAdmin.list_display += ('school', 'role', 'permissions')
+UserAdmin.list_filter += ('school', 'role', 'permissions')
+UserAdmin.fieldsets += ((None, {'fields': ['school','role', 'permissions']}),)
+UserAdmin.add_fieldsets += ((None, {'fields': ['school', 'role', 'permissions']}),)
+
+admin.site.register(User, UserAdmin)
+admin.site.register(School)
 admin.site.register(Course)
 admin.site.register(Inscription)
 admin.site.register(Resource)
--- appvr/classroomvr/models.py
+++ appvr/classroomvr/models.py
@@ -1,40 +1,49 @@
+from re import M
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-
+class Role(models.Model):
+    name = models.CharField(max_length=30)
 class School(models.Model):
-    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=50)
     def __str__(self):
         return self.name
-
+class PrivacyPolicy(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+class PrivacyPermission(models.Model):
+    description = models.CharField(max_length=100)
+    def __str__(self):
+        return self.description
+class PrivacyPolicies_PrivacyPermissions(models.Model):
+    privacyPolicy = models.ForeignKey(PrivacyPolicy, on_delete=models.CASCADE)
+    privacyPermission = models.ForeignKey(PrivacyPermission, on_delete=models.CASCADE)
+class User(AbstractUser):
+    school = models.ForeignKey(School, on_delete=models.CASCADE, blank=True, null=True)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, blank=True, null=True)
+    permissions = models.ForeignKey(PrivacyPolicy, on_delete=models.CASCADE, blank=True, null=True)
 class Course(models.Model):
-    id = models.AutoField(primary_key=True)
-    id_school = models.IntegerField()
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     def __str__(self):
         return self.name
-
 class Inscription(models.Model):
-    id_user = models.IntegerField();
-    id_course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     courseRole = models.CharField(max_length=20)
-
 class Resource(models.Model):
-    id = models.AutoField(primary_key=True)
-    id_course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     routeResource = models.CharField(max_length=100)
     def __str__(self):
         return self.name
-
 class Exercise(models.Model):
-    id = models.AutoField(primary_key=True)
-    id_course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    id_professor = models.IntegerField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    professor = models.ForeignKey(User, on_delete=models.CASCADE)
     deliveryDate = models.DateTimeField()
     file = models.CharField(max_length=100)
     score = models.IntegerField()
@@ -42,27 +51,9 @@ class Exercise(models.Model):
     studentCommentary = models.CharField(max_length=500)
     def __str__(self):
         return self.file
-
 class Pin(models.Model):
-    id = models.AutoField(primary_key=True)
-    id_exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
-    id_student = models.IntegerField()
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
     pin = models.IntegerField()
     def __str__(self):
         return self.pin
-
-class PrivacyPolicy(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    def __str__(self):
-        return self.name
-
-class PrivacyPermission(models.Model):
-    id = models.AutoField(primary_key=True)
-    description = models.CharField(max_length=100)
-    def __str__(self):
-        return self.description
-
-class PrivacyPolicies_PrivacyPermissions(models.Model):
-    id_privacyPolicy = models.ForeignKey(PrivacyPolicy, on_delete=models.CASCADE)
-    id_privacyPermission = models.ForeignKey(PrivacyPermission, on_delete=models.CASCADE)
