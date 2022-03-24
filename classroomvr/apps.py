3b54f6f42d98039a10e6cbd3e5995788ba937d00
Carlos <carvagia92@gmail.com>
2022-03-24 18:19:48 +0100

    cleaning main

--- classroomvr/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClassroomvrConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'classroomvr'
--- classroomvr/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('ejercicio/<int:courseid>/<int:taskid>/<int:userid>', views.indvidualQualification, name='index'),
-]
